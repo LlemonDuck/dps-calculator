912c6600111ff0a3b71036417d441a5b82ca2e44
LlemonDuck <napkinorton@gmail.com>
2022-06-10 22:20:26 -0400

    plugin: track last interacted npc for client-loading

--- src/test/java/com/duckblade/osrs/dpscalc/devbindings/MockClientDataProvider.java
+++ src/test/java/com/duckblade/osrs/dpscalc/devbindings/MockClientDataProvider.java
@@ -12,6 +12,7 @@ import com.duckblade.osrs.dpscalc.calc.model.WeaponCategory;
 import com.duckblade.osrs.dpscalc.plugin.osdata.clientdata.ClientDataProvider;
 import com.duckblade.osrs.dpscalc.plugin.osdata.clientdata.ClientDataProviderThreadProxy;
 import com.duckblade.osrs.dpscalc.plugin.osdata.wiki.ItemStatsProvider;
+import com.duckblade.osrs.dpscalc.plugin.osdata.wiki.NpcDataProvider;
 import com.google.common.collect.ImmutableMap;
 import java.util.Collections;
 import java.util.Map;
@@ -21,6 +22,7 @@ import javax.inject.Inject;
 import javax.inject.Singleton;
 import net.runelite.api.EquipmentInventorySlot;
 import net.runelite.api.ItemID;
+import net.runelite.api.NpcID;
 import net.runelite.api.Skill;
 
 @Singleton
@@ -47,11 +49,13 @@ public class MockClientDataProvider implements ClientDataProvider
 	}
 
 	private final ItemStatsProvider itemStatsProvider;
+	private final NpcDataProvider npcDataProvider;
 
 	@Inject
-	MockClientDataProvider(ItemStatsProvider itemStatsProvider)
+	MockClientDataProvider(ItemStatsProvider itemStatsProvider, NpcDataProvider npcDataProvider)
 	{
 		this.itemStatsProvider = itemStatsProvider;
+		this.npcDataProvider = npcDataProvider;
 	}
 
 	@Override
@@ -108,19 +112,19 @@ public class MockClientDataProvider implements ClientDataProvider
 	@Override
 	public Skills getNpcTargetSkills()
 	{
-		return null;
+		return npcDataProvider.getById(NpcID.ZULRAH).getSkills();
 	}
 
 	@Override
 	public DefensiveBonuses getNpcTargetBonuses()
 	{
-		return null;
+		return npcDataProvider.getById(NpcID.ZULRAH).getDefensiveBonuses();
 	}
 
 	@Override
 	public DefenderAttributes getNpcTargetAttributes()
 	{
-		return null;
+		return npcDataProvider.getById(NpcID.ZULRAH).getAttributes();
 	}
 
 	@Override
