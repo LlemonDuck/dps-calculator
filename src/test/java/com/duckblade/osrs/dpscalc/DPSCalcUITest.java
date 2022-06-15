b9d79997a9a37bd309eb266f86d048882be177f0
LlemonDuck <napkinorton@gmail.com>
2022-06-14 20:23:13 -0400

    test: remove v1 init code

--- src/test/java/com/duckblade/osrs/dpscalc/DPSCalcUITest.java
+++ src/test/java/com/duckblade/osrs/dpscalc/DPSCalcUITest.java
@@ -68,45 +68,7 @@ public class DPSCalcUITest
 
 			JFrame frame = new JFrame();
 			frame.getLayeredPane().setCursor(Cursor.getDefaultCursor());
-
-			DpsPluginPanel pluginPanel = testInjector.getInstance(DpsPluginPanel.class);
-
-//			NpcDataManager npcDataManager = new NpcDataManager();
-//			ItemDataManager itemDataManager = new ItemDataManager();
-//
-//			CalcManager cm = new CalcManager(new MageDpsCalc(), new MeleeDpsCalc(), new RangedDpsCalc());
-//			NpcStatsPanel nsp = new NpcStatsPanel(npcDataManager);
-//			EquipmentPanel ep = new EquipmentPanel(null, null, null, itemDataManager);
-//			SkillsPanel sp = new SkillsPanel(null, null);
-//			PrayerPanel pp = new PrayerPanel(); // heh pp
-//			CalcResultPanel rp = new CalcResultPanel();
-//
-//			DpsCalcPanel calcPanel = new DpsCalcPanel(cm, nsp, ep, sp, pp, rp);
-//
-//			 preloading (current is tbow max hit)
-//			nsp.loadNpcStats(npcDataManager.getNpcStatsById(NpcID.BRUTAL_BLACK_DRAGON));
-//			ep.setEquipment(ImmutableMap.of(
-//					EquipmentInventorySlot.WEAPON, itemDataManager.getItemStatsById(ItemID.TWISTED_BOW),
-//					EquipmentInventorySlot.AMMO, itemDataManager.getItemStatsById(ItemID.DRAGON_ARROW),
-//					EquipmentInventorySlot.AMULET, itemDataManager.getItemStatsById(ItemID.NECKLACE_OF_ANGUISH),
-//					EquipmentInventorySlot.CAPE, itemDataManager.getItemStatsById(ItemID.AVAS_ASSEMBLER),
-//					EquipmentInventorySlot.HEAD, itemDataManager.getItemStatsById(ItemID.SLAYER_HELMET_I)
-//			));
-//			ep.setOnSlayerTask(true);
-//			ep.setWeaponMode(WeaponType.BOW.getWeaponModes().get(0));
-//			sp.setSkills(new ImmutableMap.Builder<Skill, Integer>()
-//					.put(Skill.RANGED, 99)
-//					.put(Skill.ATTACK, 99)
-//					.put(Skill.STRENGTH, 99)
-//					.put(Skill.DEFENCE, 99)
-//					.put(Skill.MAGIC, 99)
-//					.put(Skill.PRAYER, 99)
-//					.build()
-//			);
-//			sp.setBoosts(ImmutableMap.of(Skill.RANGED, 13));
-//			pp.setOffensive(Prayer.RIGOUR);
-
-			frame.add(pluginPanel);
+			frame.add(testInjector.getInstance(DpsPluginPanel.class));
 
 			frame.setSize(242, 800);
 			frame.setResizable(false);
