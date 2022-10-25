49a768dbda81b2d15b07cab2be22ac0b611e3255
Paul Norton <napkinorton@gmail.com>
2022-10-25 13:14:29 -0400

    phub: remove deprecated usages

--- src/main/java/com/duckblade/osrs/dpscalc/plugin/osdata/wiki/WikiNpcDataProvider.java
+++ src/main/java/com/duckblade/osrs/dpscalc/plugin/osdata/wiki/WikiNpcDataProvider.java
@@ -16,7 +16,6 @@ import lombok.extern.slf4j.Slf4j;
 public class WikiNpcDataProvider implements NpcDataProvider
 {
 
-	private static final Gson GSON = new Gson();
 	private static final String NPC_STATS_URL = "npcs.min.json";
 	private static final String NPC_BASE_IDS_URL = "npc-base-ids.min.json";
 
@@ -24,15 +23,15 @@ public class WikiNpcDataProvider implements NpcDataProvider
 	private Map<Integer, Integer> npcBaseIdsMap;
 
 	@Inject
-	public WikiNpcDataProvider(WikiDataLoader wikiDataLoader) throws IOException
+	public WikiNpcDataProvider(Gson gson, WikiDataLoader wikiDataLoader) throws IOException
 	{
 		wikiDataLoader.getReader(NPC_BASE_IDS_URL, reader ->
-			npcBaseIdsMap = GSON.fromJson(reader, new TypeToken<HashMap<Integer, Integer>>()
+			npcBaseIdsMap = gson.fromJson(reader, new TypeToken<HashMap<Integer, Integer>>()
 			{
 			}.getType())
 		);
 		wikiDataLoader.getReader(NPC_STATS_URL, reader ->
-			npcStatsMap = GSON.fromJson(reader, new TypeToken<HashMap<Integer, NpcData>>()
+			npcStatsMap = gson.fromJson(reader, new TypeToken<HashMap<Integer, NpcData>>()
 			{
 			}.getType())
 		);
