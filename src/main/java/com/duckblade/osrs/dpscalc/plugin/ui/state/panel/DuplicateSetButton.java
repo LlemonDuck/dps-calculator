79585af0981e6db99198940d5165c46f8986e443
LlemonDuck <napkinorton@gmail.com>
2024-01-25 03:05:57 -0500

    reformatting

--- src/main/java/com/duckblade/osrs/dpscalc/plugin/ui/state/panel/DuplicateSetButton.java
+++ src/main/java/com/duckblade/osrs/dpscalc/plugin/ui/state/panel/DuplicateSetButton.java
@@ -12,25 +12,26 @@ import net.runelite.client.util.ImageUtil;
 public class DuplicateSetButton extends JButton
 {
 
-    private static final ImageIcon DELETE_ICON =
-            new ImageIcon(ImageUtil.resizeImage(ImageUtil.loadImageResource(DeleteSetButton.class, "icon_copy.png"), 20, 20));
+	private static final ImageIcon DELETE_ICON =
+		new ImageIcon(ImageUtil.resizeImage(ImageUtil.loadImageResource(DeleteSetButton.class, "icon_copy.png"), 20, 20));
 
-    @Inject
-    public DuplicateSetButton(PanelStateManager manager)
-    {
-        super(DELETE_ICON);
-        setFocusPainted(false);
-        addActionListener(e ->
-        {
-            String name = JOptionPane.showInputDialog(
-                    this,
-                    "New set name:",
-                    "Duplicate \"" + manager.currentSet().getName() + "\"",
-                    JOptionPane.QUESTION_MESSAGE);
-            if (name != null)
-            {
-                manager.duplicateSet(manager.currentSet(), name);
-            }
-        });
-    }
+	@Inject
+	public DuplicateSetButton(PanelStateManager manager)
+	{
+		super(DELETE_ICON);
+		setFocusPainted(false);
+		addActionListener(e ->
+		{
+			String name = JOptionPane.showInputDialog(
+				this,
+				"New set name:",
+				"Duplicate \"" + manager.currentSet().getName() + "\"",
+				JOptionPane.QUESTION_MESSAGE
+			);
+			if (name != null)
+			{
+				manager.duplicateSet(manager.currentSet(), name);
+			}
+		});
+	}
 }
